bbdc4423ace9fba4c5c9a3c04c5b6f8c9d488743
Vincent Howard <vincenguyenhoward@gmail.com>
2024-04-29 19:52:40 -0600

    added missing file

--- src/app/(app)/index.tsx
+++ src/app/(app)/index.tsx
@@ -1,16 +1,52 @@
-import { Text, View } from "react-native";
+import { useEffect } from "react";
+import { Text, SafeAreaView } from "react-native";
+import { useDispatch, useSelector } from "react-redux";
 import { router } from "expo-router";
+import styled from "styled-components/native";
 import { clearPersistedState } from "../../store";
+import { ROUTES } from "../../constants/routes";
+import { ThemeType } from "../../styles/theme";
+import type { RootState } from "../../store";
+import { fetchEthereumBalance } from "../../store/walletSlice";
+import type { AppDispatch } from "../../store";
+
+export const SafeAreaContainer = styled(SafeAreaView)<{ theme: ThemeType }>`
+  flex: 1;
+  background-color: ${(props) => props.theme.colors.dark};
+  justify-content: flex-end;
+`;
+
+export const ContentContainer = styled.View<{ theme: ThemeType }>`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+`;
 
 export default function Index() {
+  const dispatch = useDispatch<AppDispatch>();
+  const ethWalletAddress = useSelector(
+    (state: RootState) => state.wallet.ethereum.address
+  );
+  const ethBalance = useSelector(
+    (state: RootState) => state.wallet.ethereum.balance
+  );
   const logout = () => {
     clearPersistedState();
     router.replace("/(wallet)/wallet-setup");
   };
 
+  useEffect(() => {
+    if (ethWalletAddress) {
+      dispatch(fetchEthereumBalance(ethWalletAddress));
+    }
+  }, [ethWalletAddress, dispatch]);
+
+  console.log(ethWalletAddress);
+  console.log(ethBalance);
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+    <SafeAreaContainer>
+      <ContentContainer></ContentContainer>
       <Text onPress={() => logout()}>logout</Text>
-    </View>
+    </SafeAreaContainer>
   );
 }
